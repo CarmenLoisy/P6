875c9fa5da3e9b453b88164d978e9e1147d21889
CarmenLoisy <CarmenLoisy@users.noreply.github.com>
2023-12-07 10:35:50 +0100

    update

---
+++ FrontEnd/script.js
@@ -0,0 +1,84 @@
+// Récupération des travaux depuis le back-end
+const url = "http://localhost:5678/api/works";
+// Utilisation de fetch pour envoyer une requête GET
+fetch(url)
+	.then((response) => {
+		// Vérifier si la requête a réussi (statut 200-299)
+		if (!response.ok) {
+			throw new Error("Erreur de requête réseau");
+		}
+		return response.json(); // Convertir la réponse en JSON
+	})
+	.then((data) => {
+		// Traitement des données récupérées
+		const gallery = document.querySelector(".gallery"); //Sélection de l'élément DOM avec la classe "gallery" pour y ajouter les travaux.
+		// Utilisation de for...of pour simplifier la boucle
+		for (const elements of data) {
+			const project = createProject(elements); //Appel de la fonction à partir des données
+			gallery.appendChild(project); //ajout dans la galerie
+		}
+	})
+function createProject(elements) {
+	// Création des balises
+	const project = document.createElement("figure");
+	const img = document.createElement("img");
+	const imgTitle = document.createElement("figcaption");
+	// Configuration des éléments avec les données
+	img.src = elements.imageUrl;
+	imgTitle.innerText = elements.title;
+	// Attachement des éléments au DOM
+	project.appendChild(img);
+	project.appendChild(imgTitle);
+	// Ajout de classes et attributs
+	project.classList.add("project");
+	project.setAttribute("data-category", elements.category.name);
+
+	return project;
+}
+// Filtrage des projets
+fetch("http://localhost:5678/api/categories")
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error("Erreur de requête réseau");
+		}
+		return response.json();
+	})
+	.then((dataCategory) => {
+		// Récupérez la liste des filtres
+		const filters = document.querySelectorAll(".filtres div");
+		// Fonction pour activer le filtre
+		function setActiveFilter(filter) {
+			filters.forEach((f) => f.classList.remove("active"));
+			filter.classList.add("active");
+		}
+		// Écoutez les clics sur les boutons de filtre
+		filters.forEach((filter) => {
+			filter.addEventListener("click", () => {
+				// Récupérez le nom de la catégorie du filtre cliqué
+				const filterName = filter.textContent.trim(); // Utilisez le contenu textuel du filtre comme catégorie
+
+				// Filtrage des projets en fonction du nom de la catégorie
+				const projects = document.querySelectorAll(".project");
+
+				projects.forEach((project) => {
+					const projectCategory = project.getAttribute("data-category");
+
+					project.style.display =
+						filterName === "Tous" || filterName === projectCategory ? "block" : "none";
+				})
+				// Mettre en surbrillance le filtre actif
+				setActiveFilter(filter)
+			})
+			// Mettez en surbrillance le filtre par défaut (ici, le premier filtre)
+			if (filter.classList.contains("tous")) {
+				setActiveFilter(filter)
+			}
+		})
+	})
+
+	.catch((error) => {
+		console.error(
+			"Une erreur s'est produite lors de la récupération des catégories : ",
+			error
+		)
+	})
